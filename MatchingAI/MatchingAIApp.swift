9226326bf4b2eecd067736b8c6679f1b83d6e157
kuro48 <kuroryu.basket@gmail.com>
2025-05-31 13:03:45 +0900

    Initial Commit

---
+++ MatchingAI/MatchingAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingAIApp.swift
+//  MatchingAI
+//
+//  Created by Ryunosuke Kurokawa on 2025/05/31.
+//
+
+import SwiftUI
+
+@main
+struct MatchingAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
